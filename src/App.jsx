6d4a3f85d640328ae9652eab9407002003ab6c17
Biettt <rm551237@fiap.com.br>
2023-09-08 14:47:23 -0300

    importando routes e componentes criados

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import {} from 'react'
 import './App.css'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Nav from './Nav'
+import Home from './Home'
+import Produtos from './Produtos'
+import Sobre from './Sobre'
+import Footer from './Footer'
 
 function App() {
 

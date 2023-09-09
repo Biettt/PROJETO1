fe086ca106c5c09e1ecc43f35bcc7ce130537662
Biettt <rm551237@fiap.com.br>
2023-09-09 17:16:47 -0300

    formatando codigo

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ import './css/style.css'
 function Footer(){
 
     return(
-        <section>
+        <section className='footer'>
             <footer>Contatos</footer>
             <footer>Telefone: (11)4002-8922</footer>
             <footer>Email: gmgsg@gmail.com</footer>

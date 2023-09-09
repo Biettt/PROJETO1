4177b7d0263452104502c473350a1fdbb1cf5df2
Biettt <rm551237@fiap.com.br>
2023-09-09 18:31:26 -0300

    upando home/prod/sobre

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,21 +5,79 @@ function Home(){
 
     return(
         <>
-        <h1>Título</h1>
+        <h1>Título X</h1>
 
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur, quae? Nostrum excepturi rerum dolores similique praesentium, 
-            eligendi asperiores neque necessitatibus quos iste cumque tempore laboriosam eos quam aliquam dicta accusamus. 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet enim velit, quidem facere quibusdam, 
-            similique mollitia eos recusandae repudiandae at deserunt, cupiditate tenetur. 
-            Modi aliquid aliquam praesentium incidunt natus aspernatur?Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-            Vero quasi inventore in commodi fugit, earum voluptas consectetur minus voluptatem rem assumenda blanditiis sunt nostrum 
-            iste sint recusandae, aliquid voluptatibus eius. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Totam aspernatur veniam quos quaerat atque vero nemo obcaecati quisquam, repellendus illum accusantium laudantium possimus 
-            iste quas rerum repellat architecto quo officia. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Nemo neque eum distinctio, commodi nam accusamus adipisci inventore earum cum dolorem doloribus doloremque a soluta laudantium, 
-            necessitatibus debitis. Odio, esse dolorem. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam necessitatibus non 
-            magnam, dicta optio ipsam a ex mollitia repudiandae consequatur earum cupiditate, porro illo? Facilis aspernatur reprehenderit 
-            consectetur quam et!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dicta quam vitae expedita reprehenderit ipsa explicabo nisi, sapiente 
+            veniam dolores alias nulla voluptas totam dolore quaerat nam maxime magni suscipit. Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Quidem quas quisquam dolorem aliquam rem, ea tempora ex saepe reiciendis ab veniam sint sapiente beatae hic 
+            distinctio possimus reprehenderit ut ratione.lorem Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto odio itaque 
+            aliquam excepturi voluptatem ipsam harum iste natus libero vitae hic dolore similique animi in, ullam dignissimos perferendis quos 
+            tenetur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dicta quam vitae expedita reprehenderit ipsa explicabo nisi, 
+            sapiente veniam dolores alias nulla voluptas totam dolore quaerat nam maxime magni suscipit. Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Quidem quas quisquam dolorem aliquam rem, ea tempora ex saepe reiciendis ab veniam sint sapiente beatae hic 
+            distinctio possimus reprehenderit ut ratione. lorem Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto odio itaque 
+            aliquam excepturi voluptatem ipsam harum iste natus libero vitae hic dolore similique animi in, ullam dignissimos perferendis quos 
+            tenetur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dicta quam vitae expedita reprehenderit ipsa explicabo nisi, 
+            sapiente veniam dolores alias nulla voluptas totam dolore quaerat nam maxime magni suscipit. Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Quidem quas quisquam dolorem aliquam rem, ea tempora ex saepe reiciendis ab veniam sint sapiente beatae hic 
+            distinctio possimus reprehenderit ut ratione.lorem Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto odio itaque 
+            aliquam excepturi voluptatem ipsam harum iste natus libero vitae hic dolore similique animi in, ullam dignissimos perferendis quos 
+            tenetur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dicta quam vitae expedita reprehenderit ipsa explicabo nisi, 
+            sapiente veniam dolores alias nulla voluptas totam dolore quaerat nam maxime magni suscipit. Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Quidem quas quisquam dolorem aliquam rem, ea tempora ex saepe reiciendis ab veniam sint sapiente beatae hic 
+            distinctio possimus reprehenderit ut ratione. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto odio itaque aliquam 
+            excepturi voluptatem ipsam harum iste natus libero vitae hic dolore similique animi in, ullam dignissimos perferendis quos tenetur? 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dicta quam vitae expedita reprehenderit ipsa explicabo nisi, sapiente 
+            veniam dolores alias nulla voluptas totam dolore quaerat nam maxime magni suscipit. Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Quidem quas quisquam dolorem aliquam rem, ea tempora ex saepe reiciendis ab veniam sint sapiente beatae hic 
+            distinctio possimus reprehenderit ut ratione. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto odio itaque aliquam 
+            excepturi voluptatem ipsam harum iste natus libero vitae hic dolore similique animi in, ullam dignissimos perferendis quos tenetur? 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dicta quam vitae expedita reprehenderit ipsa explicabo nisi, sapiente 
+            veniam dolores alias nulla voluptas totam dolore quaerat nam maxime magni suscipit. Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Quidem quas quisquam dolorem aliquam rem, ea tempora ex saepe reiciendis ab veniam sint sapiente beatae hic 
+            distinctio possimus reprehenderit ut ratione.lorem Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto odio itaque 
+            aliquam excepturi voluptatem ipsam harum iste natus libero vitae hic dolore similique animi in, ullam dignissimos perferendis quos 
+            tenetur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dicta quam vitae expedita reprehenderit ipsa explicabo nisi, 
+            sapiente veniam dolores alias nulla voluptas totam dolore quaerat nam maxime magni suscipit. Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Quidem quas quisquam dolorem aliquam rem, ea tempora ex saepe reiciendis ab veniam sint sapiente beatae hic 
+            distinctio possimus reprehenderit ut ratione.lorem Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto odio itaque 
+            aliquam excepturi voluptatem ipsam harum iste natus libero vitae hic dolore similique animi in, ullam dignissimos perferendis quos 
+            tenetur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dicta quam vitae expedita reprehenderit ipsa explicabo nisi, 
+            sapiente veniam dolores alias nulla voluptas totam dolore quaerat nam maxime magni suscipit. Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Quidem quas quisquam dolorem aliquam rem, ea tempora ex saepe reiciendis ab veniam sint sapiente beatae hic 
+            distinctio possimus reprehenderit ut ratione.lorem Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto odio itaque 
+            aliquam excepturi voluptatem ipsam harum iste natus libero vitae hic dolore similique animi in, ullam dignissimos perferendis quos 
+            tenetur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dicta quam vitae expedita reprehenderit ipsa explicabo nisi, 
+            sapiente veniam dolores alias nulla voluptas totam dolore quaerat nam maxime magni suscipit. Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Quidem quas quisquam dolorem aliquam rem, ea tempora ex saepe reiciendis ab veniam sint sapiente beatae hic 
+            distinctio possimus reprehenderit ut ratione. lorem Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto odio itaque 
+            aliquam excepturi voluptatem ipsam harum iste natus libero vitae hic dolore similique animi in, ullam dignissimos perferendis quos 
+            tenetur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dicta quam vitae expedita reprehenderit ipsa explicabo nisi, 
+            sapiente veniam dolores alias nulla voluptas totam dolore quaerat nam maxime magni suscipit. Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Quidem quas quisquam dolorem aliquam rem, ea tempora ex saepe reiciendis ab veniam sint sapiente beatae hic 
+            distinctio possimus reprehenderit ut ratione.lorem Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto odio itaque 
+            aliquam excepturi voluptatem ipsam harum iste natus libero vitae hic dolore similique animi in, ullam dignissimos perferendis quos 
+            tenetur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dicta quam vitae expedita reprehenderit ipsa explicabo nisi, 
+            sapiente veniam dolores alias nulla voluptas totam dolore quaerat nam maxime magni suscipit. Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Quidem quas quisquam dolorem aliquam rem, ea tempora ex saepe reiciendis ab veniam sint sapiente beatae hic 
+            distinctio possimus reprehenderit ut ratione. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto odio itaque aliquam 
+            excepturi voluptatem ipsam harum iste natus libero vitae hic dolore similique animi in, ullam dignissimos perferendis quos tenetur? 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dicta quam vitae expedita reprehenderit ipsa explicabo nisi, sapiente 
+            veniam dolores alias nulla voluptas totam dolore quaerat nam maxime magni suscipit. Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Quidem quas quisquam dolorem aliquam rem, ea tempora ex saepe reiciendis ab veniam sint sapiente beatae hic 
+            distinctio possimus reprehenderit ut ratione. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto odio itaque aliquam 
+            excepturi voluptatem ipsam harum iste natus libero vitae hic dolore similique animi in, ullam dignissimos perferendis quos tenetur? 
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dicta quam vitae expedita reprehenderit ipsa explicabo nisi, sapiente 
+            veniam dolores alias nulla voluptas totam dolore quaerat nam maxime magni suscipit. Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Quidem quas quisquam dolorem aliquam rem, ea tempora ex saepe reiciendis ab veniam sint sapiente beatae hic 
+            distinctio possimus reprehenderit ut ratione.lorem Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto odio itaque 
+            aliquam excepturi voluptatem ipsam harum iste natus libero vitae hic dolore similique animi in, ullam dignissimos perferendis quos 
+            tenetur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero dicta quam vitae expedita reprehenderit ipsa explicabo nisi, 
+            sapiente veniam dolores alias nulla voluptas totam dolore quaerat nam maxime magni suscipit. Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Quidem quas quisquam dolorem aliquam rem, ea tempora ex saepe reiciendis ab veniam sint sapiente beatae hic 
+            distinctio possimus reprehenderit ut ratione.lorem Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto odio itaque 
+            aliquam excepturi voluptatem ipsam harum iste natus libero vitae hic dolore similique animi in, ullam dignissimos perferendis quos 
+            tenetur?</p>
         </>
     )
 }
--- src/Produtos.jsx
+++ src/Produtos.jsx
@@ -5,6 +5,17 @@ function Produtos(){
 
     return(
         <>
+        <img src='./src/assets/' alt='Imagem'/>
+        <p>Imagem 1</p>
+
+        <img src='./src/assets/' alt='Imagem2'/>
+        <p>Imagem 2</p>
+
+        <img src='./src/assets/' alt='Imagem3'/>
+        <p>Imagem 3</p>
+
+        <img src='./src/assets/' alt='Imagem4'/>
+        <p>Imagem 4</p>
         </>
     )
 }

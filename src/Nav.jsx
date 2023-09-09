88f603424cc028b4bb8668797b0865f06599437b
Biettt <rm551237@fiap.com.br>
2023-09-09 16:56:19 -0300

    criando nav dos componentes

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -5,6 +5,16 @@ function Nav(){
 
     return(
         <>
+        <header className='menu'>
+            <h1 id='título'>Título X</h1>
+            <nav id='menu-nav'>
+                <ul>
+                    <NavLink to='Home' id='tlink'>Home</NavLink>
+                    <NavLink to='Produtos' id='tlink'>Produtos</NavLink>
+                    <NavLink to='Sobre' id='tlink'>Sobre</NavLink>
+                </ul>
+            </nav>
+        </header>
         </>
     )
 }
